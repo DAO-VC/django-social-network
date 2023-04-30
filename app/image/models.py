dac68db6a2a3cbf485cbbffd564c233479fcf0c6
alexeygalt <alexeygalt@gmail.com>
2023-04-30 12:44:54 +0300

    validators background and logo  added

--- app/image/models.py
+++ app/image/models.py
@@ -2,18 +2,31 @@ from django.db import models
 
 from versatileimagefield.fields import VersatileImageField, PPOIField
 
-from image.validators import validate_file_size, validate_file_extension
+from image.validators import (
+    validate_file_size,
+    validate_file_extension,
+    validate_image_extension,
+    validate_image_size,
+)
 
 
 class Background(models.Model):
     background = VersatileImageField(
-        "Background", upload_to="backgrounds/", ppoi_field="background_ppoi"
+        "Background",
+        upload_to="backgrounds/",
+        ppoi_field="background_ppoi",
+        validators=[validate_image_extension, validate_image_size],
     )
     background_ppoi = PPOIField()
 
 
 class Logo(models.Model):
-    logo = VersatileImageField("Logo", upload_to="logos/", ppoi_field="logo_ppoi")
+    logo = VersatileImageField(
+        "Logo",
+        upload_to="logos/",
+        ppoi_field="logo_ppoi",
+        validators=[validate_image_extension, validate_image_size],
+    )
     logo_ppoi = PPOIField()
 
 
--- app/image/validators.py
+++ app/image/validators.py
@@ -16,3 +16,19 @@ def validate_file_extension(value):
     valid_extensions = [".pdf"]
     if not ext.lower() in valid_extensions:
         raise ValidationError("Unsupported file extension.")
+
+
+def validate_image_size(value):
+    filesize = value.size
+
+    if filesize > 6485760:
+        raise ValidationError("You cannot upload logo more than 6Mb")
+    else:
+        return value
+
+
+def validate_image_extension(value):
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = [".jpg", ".png", "gif"]
+    if not ext.lower() in valid_extensions:
+        raise ValidationError("Unsupported logo extension.")
