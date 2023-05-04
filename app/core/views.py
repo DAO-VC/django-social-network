e71f2c5283dc9bcb7883ca275fb1a1bdcf8198f4
alexeygalt <alexeygalt@gmail.com>
2023-05-04 11:26:24 +0300

    fixed

--- app/core/views.py
+++ app/core/views.py
@@ -45,7 +45,7 @@ class UserRegistrationView(CreateAPIView):
 
 # @method_decorator(csrf_exempt, name="dispatch")
 # @method_decorator(ensure_csrf_cookie, name="dispatch")
-@csrf_exempt
+@method_decorator(csrf_exempt, name="dispatch")
 class UserLoginView(APIView):
     """Логин пользователя"""
 
