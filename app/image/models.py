f7a9b04af3440b59da6cf351b37faa5a6f67d8ce
alexeygalt <alexeygalt@gmail.com>
2023-05-17 11:37:53 +0300

    file valid deleted

--- app/image/models.py
+++ app/image/models.py
@@ -9,9 +9,9 @@ class Image(models.Model):
     image = models.FileField(
         "Background",
         upload_to="images/",
-        validators=[validate_image_size],
+        # validators=[validate_image_size],
     )
 
 
 class File(models.Model):
-    pdf = models.FileField(upload_to="files/", validators=[validate_file_size])
+    pdf = models.FileField(upload_to="files/")
