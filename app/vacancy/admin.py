e37c378eade971fffec6d0b6bfccd195fce84280
alexeygalt <alexeygalt@gmail.com>
2023-05-24 10:27:49 +0300

    models in admin added

--- app/vacancy/admin.py
+++ app/vacancy/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from vacancy.models import Vacancy, WorkTeam
+from vacancy.models import Vacancy, WorkTeam, Candidate, Offer
 
 admin.site.register(Vacancy)
 admin.site.register(WorkTeam)
+admin.site.register(Candidate)
+admin.site.register(Offer)
