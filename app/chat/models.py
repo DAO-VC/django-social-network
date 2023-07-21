5295e4e06eff7c525d15403c8734f210e76d9251
alexeygalt <alexeygalt@gmail.com>
2023-07-21 11:58:38 +0300

    notif model changed

--- app/chat/models.py
+++ app/chat/models.py
@@ -65,12 +65,15 @@ class ChatNotification(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        # on_delete=models.CASCADE,
         related_name="reciever",
         verbose_name="Получатель уведомления",
+        null=True,
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        # on_delete=models.CASCADE,
         related_name="author",
         null=True,
         verbose_name="Автор(опционально)",
