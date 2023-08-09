31e7d82eba9ab99b4a0bc6819acd52a048007b82
alexeygalt <alexeygalt@gmail.com>
2023-08-09 09:41:01 +0300

    daphne added

--- app/main/asgi.py
+++ app/main/asgi.py
@@ -1,18 +1,21 @@
 import os
-from channels_auth_token_middlewares.middleware import (
-    QueryStringSimpleJWTAuthTokenMiddleware,
-)
 
-from chat import routing as chat_routing
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
+django_asgi_app = get_asgi_application()
+from chat import routing as chat_routing
+from channels_auth_token_middlewares.middleware import (
+    QueryStringSimpleJWTAuthTokenMiddleware,
+)
+
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             QueryStringSimpleJWTAuthTokenMiddleware(
                 URLRouter(chat_routing.websocket_urlpatterns)
