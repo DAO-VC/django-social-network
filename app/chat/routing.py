35a254fc14d4d292a954c034eacecaf0cebea2df
alexeygalt <alexeygalt@gmail.com>
2023-08-09 10:36:41 +0300

    urls fix

--- app/chat/routing.py
+++ app/chat/routing.py
@@ -5,7 +5,13 @@ from .consumers import NotificationConsumer, OnlineStatusConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
-    path("ws/notify/", NotificationConsumer.as_asgi()),
+    path("/notify/", NotificationConsumer.as_asgi()),
     # path(r"ws/online/(?P<chat_id>\w+)/$", OnlineStatusConsumer.as_asgi()), ]
-    path("ws/online/", OnlineStatusConsumer.as_asgi()),
+    path("/online/", OnlineStatusConsumer.as_asgi()),
 ]
+
+
+# re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+#     path("ws/notify/", NotificationConsumer.as_asgi()),
+#     # path(r"ws/online/(?P<chat_id>\w+)/$", OnlineStatusConsumer.as_asgi()), ]
+#     path("ws/online/", OnlineStatusConsumer.as_asgi()),
