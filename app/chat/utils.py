91cadf818ee93a752b700212c2a4afed8346e98e
alexeygalt <alexeygalt@gmail.com>
2023-09-08 11:22:43 +0300

    minor fix

--- app/chat/utils.py
+++ app/chat/utils.py
@@ -1,14 +1,20 @@
+from django.db.models import Q
+
 from core.admin import User
 from chat.models import Room
 
 
 def banned_user_chats(status: str, author: User, receiver: User) -> None:
     if status == "banned":
-        for room in Room.objects.filter(author=author, receiver=receiver):
+        for room in Room.objects.filter(author=author, receiver=receiver) | Q(
+            author=receiver, receiver=author
+        ):
             room.status = Room.ChatStatus.BANNED
             room.save()
 
     else:
-        for room in Room.objects.filter(author=author, receiver=receiver):
+        for room in Room.objects.filter(author=author, receiver=receiver) | Q(
+            author=receiver, receiver=author
+        ):
             room.status = Room.ChatStatus.ACTIVE
             room.save()
