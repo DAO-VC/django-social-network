ce95c986e181a1e00baf6522ef42641cb93355f1
alexeygalt <alexeygalt@gmail.com>
2023-09-08 11:31:03 +0300

    minor fix

--- app/chat/utils.py
+++ app/chat/utils.py
@@ -6,15 +6,15 @@ from chat.models import Room
 
 def banned_user_chats(status: str, author: User, receiver: User) -> None:
     if status == "banned":
-        for room in Room.objects.filter(author=author, receiver=receiver) | Q(
-            author=receiver, receiver=author
+        for room in Room.objects.filter(
+            Q(author=author, receiver=receiver) | Q(author=receiver, receiver=author)
         ):
             room.status = Room.ChatStatus.BANNED
             room.save()
 
     else:
-        for room in Room.objects.filter(author=author, receiver=receiver) | Q(
-            author=receiver, receiver=author
+        for room in Room.objects.filter(
+            Q(author=author, receiver=receiver) | Q(author=receiver, receiver=author)
         ):
             room.status = Room.ChatStatus.ACTIVE
             room.save()
