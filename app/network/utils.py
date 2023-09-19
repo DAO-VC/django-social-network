563b9e4e590dcc1398c3b50c5d792ae024ce4868
alexeygalt <alexeygalt@gmail.com>
2023-09-19 15:55:14 +0300

    test celery

--- app/network/utils.py
+++ app/network/utils.py
@@ -10,7 +10,12 @@ class ConnectNetwork(object):
         for network in all_networks_queryset:
             while True:
                 random_network_id = random.choice(list_id_networks)
-                if random_network_id != network.connect_network.id:
+                if network.connect_network:
+                    if random_network_id != network.connect_network.id:
+                        network.connect_network.id = random_network_id
+                        network.save()
+                        break
+                else:
                     network.connect_network.id = random_network_id
                     network.save()
                     break
