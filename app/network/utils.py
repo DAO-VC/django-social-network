b0f2fba021ffc521ff205b3cff70178c45a03d2a
alexeygalt <alexeygalt@gmail.com>
2023-09-19 16:33:07 +0300

    test celery

--- app/network/utils.py
+++ app/network/utils.py
@@ -5,17 +5,17 @@ import random
 class ConnectNetwork(object):
     def connect_all(self) -> None:
         all_networks_queryset = Network.objects.all()
-        list_id_networks = [item.id for item in Network.objects.all()]
+        # list_id_networks = [item for item in Network.objects.all()]
 
         for network in all_networks_queryset:
             while True:
-                random_network_id = random.choice(list_id_networks)
+                random_network = random.choice(all_networks_queryset)
                 if network.connect_network:
-                    if random_network_id != network.connect_network.id:
-                        network.connect_network.id = random_network_id
+                    if random_network.id != network.connect_network.id:
+                        network.connect_network.id = random_network.id
                         network.save()
                         break
                 else:
-                    network.connect_network = random_network_id
+                    network.connect_network = random_network
                     network.save()
                     break
